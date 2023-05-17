9076601c63ae318ea630f1e8c445bc26a2672261
Anzhi <2363299334@qq.com>
2023-05-17 19:33:38 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author Anzhi
+ * @data 2023/5/17 - 19:29
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
